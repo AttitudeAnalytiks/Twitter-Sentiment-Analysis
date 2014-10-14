5493879d7c31770296cecdbb617c78efe22b2ff1
AttitudeAnalytiks <kcoconor@hotmail.com>
2014-10-14 14:59:22 -0400

    Create getTweets.py
    Get live tweets
    
    This module establishes a connection with Twitter streaming API and downloads live tweets that are in English. The tweets are stored in 'tweets.json' .

---
+++ getTweets.py
@@ -0,0 +1,43 @@
+import oauth2 as oauth
+import urllib2 as urllib
+import json
+
+## Authorization
+api_key = "***"  
+api_secret = "**"
+access_token_key = "*-**"
+access_token_secret = "**"
+oauth_token = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+## Send request to url
+url = "https://stream.twitter.com/1.1/statuses/sample.json"
+req = oauth.Request.from_consumer_and_token(oauth_consumer, token=oauth_token, 
+                                            http_method="GET", http_url=url)
+req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+url = req.to_url()
+
+## Open the connection
+opener = urllib.OpenerDirector()
+http_handler = urllib.HTTPHandler(debuglevel=0)
+https_handler = urllib.HTTPSHandler(debuglevel=0)
+opener.add_handler(http_handler)
+opener.add_handler(https_handler)
+
+## Get the response
+response = opener.open(url)
+
+## Write response to file
+f = open('tweets.json', 'w')
+i = 0
+for line in response:
+  entry = json.loads(line)
+  ## Keep English tweets only
+  if u'lang' in entry and entry[u'lang'] == 'en' and u'text' in entry:
+    json.dump(entry, f)
+    f.write('\n')
+    i += 1
+  if i > 125000:
+    break
+f.close()
